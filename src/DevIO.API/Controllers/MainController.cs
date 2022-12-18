6e5531c533ef06281f380dc03b246b86c83201fb
BrunoEduardo <bruno.e.otk@gmail.com>
2022-12-18 19:42:00 +0000

    MainController
    criação da controller principal abstrata para implementar as controllers que serão adicionadas

---
+++ src/DevIO.API/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de errp
+
+        // validacção de modelstate
+
+        // valida de operação de negócio
+    }
+}
