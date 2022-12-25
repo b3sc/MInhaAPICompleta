b6c90bcfd196120af3ef76f33f8d04fccfe53b0a
BrunoEduardo <bruno.e.otk@gmail.com>
2022-12-25 22:14:34 +0000

    Create ApiConfig.cs
    configuração CORS

---
+++ src/DevIO.API/Configuration/ApiConfig.cs
@@ -0,0 +1,37 @@
+﻿namespace DevIO.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfig(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfig(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseCors("Development");
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseCors("Development"); // Usar apenas nas demos => Configuração Ideal: Production
+                app.UseHsts();
+            }
+
+            return app;
+
+        }
+    }
+}
