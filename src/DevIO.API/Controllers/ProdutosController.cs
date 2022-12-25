7d1e411f833a16798c252c32e3f5a60a561b6aa8
BrunoEduardo <bruno.e.otk@gmail.com>
2022-12-25 18:49:22 +0000

    Create ProdutosController.cs
    Criado produtos controller e salvando as imagens em base 64

---
+++ src/DevIO.API/Controllers/ProdutosController.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using DevIO.API.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.API.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(INotificador notificador,
+                                  IProdutoService produtoService,
+                                  IProdutoRepository produtoRepository,
+                                  IMapper mapper) : base(notificador)
+        {
+            _produtoService = produtoService;
+            _produtoRepository = produtoRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null) return NotFound();
+
+            return produtoViewModel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imagemNome = $"{Guid.NewGuid()}_{produtoViewModel.Imagem}";
+            if (!UploadArquivoWithBase64(produtoViewModel.ImagemUpload, imagemNome))
+            {
+                return CustomResponse(produtoViewModel);
+            }
+
+            produtoViewModel.Imagem = imagemNome;
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel == null) return NotFound();
+
+            await _produtoService.Remover(id);
+
+            return CustomResponse(produtoViewModel);
+        }
+
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id)
+        {
+            return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(id));
+        }
+
+        private bool UploadArquivoWithBase64(string arquivo, string imgNome)
+        {
+            if (string.IsNullOrEmpty(arquivo))
+            {
+                // --> Pode ser lançado dentro da modelState
+                //ModelState.AddModelError(string.Empty, "Forneça uma imagem para este produto!");
+
+                //ou
+
+                // Diretamente no método criado para lançar os erros.
+                NotificarErro("Forneça uma imagem para este produto!");
+                return false;
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                NotificarErro("Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
+
+            System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+            return true;
+
+        }
+
+
+
+    }
+}
